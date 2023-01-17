14bb8f9861bd873d1b6827657b2eda9db8684064
Oyinloluwa Odedoyin <odedoyinoyinloluwa@gmail.com>
2023-01-17 17:34:57 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const amount = 19;
+if (amount < 10) {
+  console.log("small Number");
+} else {
+  console.log("large Number");
+}
+console.log(`hey its my first node app !!!`);
---
+++ 3-module.js
@@ -0,0 +1,9 @@
+// const names = require("./4-names");
+// const sayHi = require("./5-utils");
+// console.log(names);
+// sayHi("susan");
+// sayHi(names.peter);
+// sayHi(names.john);
+// const data = require("./6-alternativeflavor");
+require("./7-mindgrenade");
+// console.log(data);
